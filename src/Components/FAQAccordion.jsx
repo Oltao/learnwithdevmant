353ef88637e77e6a354fcc5016ff9e2d0dbdfaf6
Olajire Taofeek <oolajire5@gmail.com>
2024-04-29 16:39:51 +0100

    Changed faq font and reduce the margin on mobile

--- src/Components/FAQAccordion.jsx
+++ src/Components/FAQAccordion.jsx
@@ -5,11 +5,11 @@ const FAQAccordion = ({ question, answer }) => {
 
   return (
     <div
-      className={`bg-white shadow shadow-neutral-500 p-5 my-5 mx-20 font-roboto lg:mx-[300px]`} data-aos="fade-up"
+      className={`bg-white shadow shadow-neutral-500 p-5 my-5 mx-5 font-roboto lg:mx-[300px]`} data-aos="fade-up"
       data-aos-duration="1500"
     >
       <h3
-        className={`font-bold text-xl text-[#178673] cursor-pointer flex justify-between`}
+        className={`font-bold text-x text-[#178673] cursor-pointer flex justify-between font-poppins`}
         onClick={() => setShowAccordion(!showAccordion)}
       >
         {question}
